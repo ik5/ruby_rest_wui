d2e9ee5fbf91a5c15e7b13ac4080e18bfe9b1156
ik5 <idok@linesip.com>
2014-12-19 21:42:21 +0200

    better knockout

--- public/js/rest_client.js
+++ public/js/rest_client.js
@@ -1,8 +1,8 @@
 
-var restClient = function() {
+function RestRoutingViewModel() {
     var self = this;
 
-    self.method              = ko.observable('GET');
+    self.method              = ko.observable();
     self.address             = ko.observable();
     self.format              = ko.observable();
     self.content             = ko.observable();
@@ -10,8 +10,8 @@ var restClient = function() {
     self.return_code         = ko.observable();
     self.return_content_type = ko.observable();
     self.answer              = ko.observable();
-};
+}
 
 $(function(){
-    ko.applyBindings(new restClient());
+    ko.applyBindings(new RestRoutingViewModel());
 });
