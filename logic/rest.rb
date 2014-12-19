f03342f9462224c3ab3e5a017dc7ac1a50358eb9
ik5 <idok@linesip.com>
2014-12-20 01:28:08 +0200

    request now returnning values

--- logic/rest.rb
+++ logic/rest.rb
@@ -1,7 +1,9 @@
 
 class REST < Sinatra::Base
+  require 'uri'
+  require 'net/http'
 
-  METHODS = %q{GET POST PUT DELETE OPTIONS HEAD TRACE CONNECT}
+  METHODS = %q{GET POST PUT DELETE OPTIONS HEAD TRACE}
 
   configure do
     enable :logging, :sessions
@@ -22,11 +24,67 @@ class REST < Sinatra::Base
     haml :index
   end
 
+  def build_method(method:, uri:)
+    m = case method
+        when 'GET'
+          Net::HTTP::Get.new(uri)
+        when 'POST'
+          Net::HTTP::Post.new(uri)
+        when 'PUT'
+          Net::HTTP::Put.new(uri)
+        when 'DELETE'
+          Net::HTTP::Delete.new(uri)
+        when 'OPTIONS'
+          Net::HTTP::Options.new(uri)
+        when 'HEAD'
+          Net::HTTP::Head.new(uri)
+        when 'TRACE'
+          Net::HTTP::Trace.new(uri)
+        end
+
+    m
+  end
+
+  def make_connection(method:, address:, format:, content:)
+    uri  = URI.parse(address)
+    http = Net::HTTP.new(uri.host, uri.port)
+    res  = build_method(method: method, uri: uri)
+
+    logger.info "format: #{format}"
+    res.content_type = format  unless format.empty?
+    res.body         = content unless content.empty?
+    answer = http.request(res)
+
+    result                  = {return_code: answer.code}
+    result[:content_type]   = answer['content-type'] if answer.key? 'content-type'
+    result[:body]           = answer['body']
+    result[:content_length] = answer['content-length'] if answer.key? 'content-length'
+    result[:all_headers]    = answer.to_hash
+    result.to_json
+  end
+
   post '/request' do
+    logger.info params.inspect
     method  = params['method'] || ''
     address = params['address'] || ''
     format  = params['format'] || ''
     content = params['content'] || ''
+
+    content_type 'application/json'
+    unless METHODS.include? method
+      return {error: 'Unknown method type was provided'}.to_json
+    end
+
+    unless address =~ URI::DEFAULT_PARSER.regexp[:ABS_URI]
+      return {error: 'Invalid address was provided'}.to_json
+    end
+
+
+    unless format.empty? || MIME::Types.include?(format)
+      return {error: 'Unknown Mime Type'}.to_json
+    end
+
+    make_connection(method: method, address: address, format: format, content: content)
   end
 
 end
