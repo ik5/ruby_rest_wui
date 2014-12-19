25b7c616ebd104be6388656b973975e743c57457
ik5 <idok@linesip.com>
2014-12-19 19:24:22 +0200

    working on testing the REST thingy

--- logic/rest.rb
+++ logic/rest.rb
@@ -20,4 +20,69 @@ class REST < Sinatra::Base
     haml :index
   end
 
+  get '/test_json' do
+    content_type 'application/json'
+    result =  { test: 'ack', params: nil }
+    result[:params] = params unless params.keys.empty?
+
+    result
+  end
+
+  get '/test_xml' do
+    content_type 'application/xml'
+    builder = Nokogiri::XML::Builder.new do |xml|
+      xml.root do
+        xml.test do
+          params.each_pair do |k,v|
+            xml.params(key: k, value: v)
+          end
+        end
+      end
+
+      builder.to_xml
+    end
+  end
+
+  post '/test_post' do
+    result = ''
+    params.each_pair do |k,v|
+      result << Rack::Utils.escape_html(k) + '=' + Rack::Utils.escape_html(v)
+    end
+
+    result
+  end
+
+  post '/test_json' do
+    params.to_json
+  end
+
+  post '/test_xml' do
+    builder = Nokogiri::XML::Builder.new do |xml|
+      xml.params do
+        params.each_pair do |k,v|
+          xml.param(key: k, value: v)
+        end
+    end
+
+    builder
+  end
+
+  put '/test_put/:id' do |id|
+  end
+
+  put '/test_json/:id' do |id|
+  end
+
+  put '/test_xml/:id' do |id|
+  end
+
+  delete '/test_delete/:id' do |id|
+  end
+
+  delete '/test_json/:id' do |id|
+  end
+
+  delete '/test_xml/:id' do |id|
+  end
+
 end
