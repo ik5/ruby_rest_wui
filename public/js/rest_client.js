842ffd2ee303f210a035b36e0f5040f672e4f22d
ik5 <idok@linesip.com>
2014-12-19 21:33:58 +0200

    working on the actual fields required to bind

--- public/js/rest_client.js
+++ public/js/rest_client.js
@@ -2,10 +2,14 @@
 var restClient = function() {
     var self = this;
 
-    self.method  = ko.observable('');
-    self.address = ko.observable('');
-    self.format  = ko.observable('');
-    self.content = ko.observable('');
+    self.method              = ko.observable('GET');
+    self.address             = ko.observable();
+    self.format              = ko.observable();
+    self.content             = ko.observable();
+
+    self.return_code         = ko.observable();
+    self.return_content_type = ko.observable();
+    self.answer              = ko.observable();
 };
 
 $(function(){
