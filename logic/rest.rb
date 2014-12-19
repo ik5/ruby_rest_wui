3e06451fabce292491270964878908e706e2f0f9
ik5 <idok@linesip.com>
2014-12-19 19:54:54 +0200

    having a REST testing paths

--- logic/rest.rb
+++ logic/rest.rb
@@ -20,20 +20,27 @@ class REST < Sinatra::Base
     haml :index
   end
 
-  get '/test_get' do
-    'test'
+  def plain_answer(params)
+    result = 'test'
+    params.each_pair do |k,v|
+      result << "\n<br>\n" + Rack::Utils.escape_html(k) + '='
+      if v.kind_of? Array
+        result << '[' + v.map{|i| Rack::Utils.escape_html(i)}.join(',') + ']'
+      else
+        result << Rack::Utils.escape_html(v)
+      end
+    end
+    result
   end
 
-  get '/test_json' do
-    content_type 'application/json'
+  def plain_json(params)
     result =  { test: 'ack', params: nil }
     result[:params] = params unless params.keys.empty?
 
     result.to_json
   end
 
-  get '/test_xml' do
-    content_type 'application/xml'
+  def plain_xml(params)
     builder = Nokogiri::XML::Builder.new do |xml|
       xml.root do
         xml.test do
@@ -43,51 +50,70 @@ class REST < Sinatra::Base
         end
       end
 
-      builder.to_xml
     end
+    builder.to_xml
+  end
+
+  get '/test_get' do
+    plain_answer(params)
+  end
+
+  get '/test_json' do
+    content_type 'application/json'
+    plain_json(params)
+  end
+
+  get '/test_xml' do
+    content_type 'application/xml'
+    plain_xml(params)
   end
 
   post '/test_post' do
-    result = ''
-    params.each_pair do |k,v|
-      result << Rack::Utils.escape_html(k) + '=' + Rack::Utils.escape_html(v)
-    end
-
-    result
+    plain_answer(params)
   end
 
   post '/test_json' do
-    params.to_json
+    content_type 'application/json'
+    plain_json(params)
   end
 
   post '/test_xml' do
-    builder = Nokogiri::XML::Builder.new do |xml|
-      xml.params do
-        params.each_pair do |k,v|
-          xml.param(key: k, value: v)
-        end
-      end
-    end
-
-    builder
+    content_type 'application/xml'
+    plain_xml(params)
   end
 
   put '/test_put/:id' do |id|
+    params['path_id'] = id
+    plain_answer(params)
   end
 
   put '/test_json/:id' do |id|
+    content_type 'application/json'
+    params['path_id'] = id
+    plain_json(params)
   end
 
   put '/test_xml/:id' do |id|
+    content_type 'application/xml'
+    params['path_id'] = id
+    plain_xml(params)
   end
 
   delete '/test_delete/:id' do |id|
+    params['path_id'] = id
+    plain_answer(params)
   end
 
   delete '/test_json/:id' do |id|
+    content_type 'application/json'
+    params['path_id'] = id
+    plain_json(params)
   end
 
   delete '/test_xml/:id' do |id|
+    content_type 'application/xml'
+    params['path_id'] = id
+    plain_xml(params)
   end
 
 end
