653fdd896060212420001998ca3d47a58c701530
ik5 <idok@linesip.com>
2014-12-19 20:15:32 +0200

    working on basic knockout controller

--- public/js/rest_client.js
+++ public/js/rest_client.js
@@ -1,8 +1,13 @@
 
 var restClient = function() {
+    var self = this;
 
-}
+    self.method  = ko.observable('');
+    self.address = ko.observable('');
+    self.format  = ko.observable('');
+    self.content = ko.observable('');
+};
 
 $(function(){
-    ko.applyBindings(new restClient())
+    ko.applyBindings(new restClient());
 });
