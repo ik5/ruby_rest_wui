7c72e45b2062380af0b742ca94c09a3d85feeb79
ik5 <idok@linesip.com>
2014-12-19 15:36:19 +0200

    basic logic code

---
+++ logic/rest.rb
@@ -0,0 +1,22 @@
+
+class REST < Sinatra::Base
+
+  configure do
+    enable :logging, :sessions
+    set :root, File.expand_path(File.join('../',File.basename(__FILE__)))
+    set :public_folder, 'public'
+    set :views, 'views'
+
+    register Sinatra::Partial
+    set :partial_template_engine, :haml
+  end
+
+  configure :development do
+    require 'sinatra/reloader'
+    register Sinatra::Reloader
+  end
+
+  get '/' do
+  end
+
+end
