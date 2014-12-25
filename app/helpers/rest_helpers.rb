72ebe827de99e6bef5d14ca0675e852a125bd85c
ik5 <idok@linesip.com>
2014-12-25 11:02:39 +0200

    moved stuff to helpers

---
+++ app/helpers/rest_helpers.rb
@@ -0,0 +1,92 @@
+
+require 'uri'
+require 'net/http'
+
+METHODS = %q{GET POST PUT DELETE OPTIONS HEAD TRACE}
+module Sinatra
+
+  module RESTHelper
+
+    def escpae_uri(parameters)
+      parameters.map{|i| i.split('=').map{|a| URI.escape(a)}.join('=') }.join('&')
+    end
+
+    def params_to_hash(parameters={})
+      params = {}
+      parameters.each do |i|
+        k,v = i.split('=')
+        if params.key? k # array ?
+          if params[k].kind_of? Array #already an array ?
+            params[k] = params[k] + [v]
+          else
+            params[k] = [params[k], v]
+          end
+        else # not an array
+          params[k] = v
+        end
+      end
+
+      params
+    end
+
+    def build_method(method:, uri:, parameters: nil)
+      resource = case method
+                 when 'GET'
+                   escaped    = escpae_uri(parameters) unless parameters.nil?
+                   escpaed  ||= ''
+                   uri.query  = escaped
+                   Net::HTTP::Get.new(uri)
+                 when 'POST'
+                   Net::HTTP::Post.new(uri)
+                 when 'PUT'
+                   Net::HTTP::Put.new(uri)
+                 when 'DELETE'
+                   Net::HTTP::Delete.new(uri)
+                 when 'OPTIONS'
+                   Net::HTTP::Options.new(uri)
+                 when 'HEAD'
+                   Net::HTTP::Head.new(uri)
+                 when 'TRACE'
+                   Net::HTTP::Trace.new(uri)
+                 end
+
+      if (method == 'POST' || method == 'PUT') && parameters
+        params = params_to_hash(parameters)
+        resource.set_form_data(params)
+      end
+      resource
+    end
+
+    def make_connection(method:, address:, format:, content:, parameters: nil)
+      uri  = URI.parse(address)
+      http = Net::HTTP.new(uri.host, uri.port)
+      res  = build_method(method: method, uri: uri, parameters: parameters)
+
+      res.content_type = format  unless format.empty?
+      res.body         = content unless content.empty?
+      answer = http.request(res)
+
+      result = []
+      result << {key: :code, value: answer.code}
+      result << {key: :content_type, value: answer['content-type']}    if answer.key? 'content-type'
+      result << {key: :content_length, value:answer['content-length']} if answer.key? 'content-length'
+      result << {key: :body, value: answer.body}
+      result << {key: :uri, value: answer.uri}
+      result << {key: :conten_range, value: answer.content_range}      if answer.content_range
+      result << {key: :range, value: answer.range}                     if answer.range
+      result << {key: :range_length, value: answer.range_length}       if answer.range_length
+      result << {key: :chunked?, value: answer.chunked?}
+      result << {key: :length, value: answer.length}
+      result << {key: :sub_type, value: answer.sub_type}
+      result << {key: :msg, value: answer.msg}
+      answer.to_hash.each_pair do |k,v|
+        result << {key: k, value: v}
+      end
+      result.to_json
+      #rescue => e
+      #  {error: "Exception was raised: #{e.message}"}.to_json
+    end
+
+  end
+
+end
--- app/logic/rest.rb
+++ app/logic/rest.rb
@@ -1,9 +1,7 @@
-
-METHODS = %q{GET POST PUT DELETE OPTIONS HEAD TRACE}
+require_relative '../helpers/rest_helpers'
 
 class REST < Sinatra::Base
-  require 'uri'
-  require 'net/http'
+  helpers Sinatra::RESTHelper
 
   configure do
     enable :logging, :sessions
@@ -22,86 +20,6 @@ class REST < Sinatra::Base
 
   get '/' do
     haml :index
-  end
-
-  def escpae_uri(parameters)
-    parameters.map{|i| i.split('=').map{|a| URI.escape(a)}.join('=') }.join('&')
-  end
-
-  def params_to_hash(parameters={})
-    params = {}
-    parameters.each do |i|
-      k,v = i.split('=')
-      if params.key? k # array ?
-        if params[k].kind_of? Array #already an array ?
-          params[k] = params[k] + [v]
-        else
-          params[k] = [params[k], v]
-        end
-      else # not an array
-        params[k] = v
-      end
-    end
-
-    params
-  end
-
-  def build_method(method:, uri:, parameters: nil)
-    resource = case method
-        when 'GET'
-          escaped    = escpae_uri(parameters) unless parameters.nil?
-          escpaed  ||= ''
-          uri.query  = escaped
-          Net::HTTP::Get.new(uri)
-        when 'POST'
-          Net::HTTP::Post.new(uri)
-        when 'PUT'
-          Net::HTTP::Put.new(uri)
-        when 'DELETE'
-          Net::HTTP::Delete.new(uri)
-        when 'OPTIONS'
-          Net::HTTP::Options.new(uri)
-        when 'HEAD'
-          Net::HTTP::Head.new(uri)
-        when 'TRACE'
-          Net::HTTP::Trace.new(uri)
-        end
-
-    if (method == 'POST' || method == 'PUT') && parameters
-      params = params_to_hash(parameters)
-      resource.set_form_data(params)
-    end
-    resource
-  end
-
-  def make_connection(method:, address:, format:, content:, parameters: nil)
-    uri  = URI.parse(address)
-    http = Net::HTTP.new(uri.host, uri.port)
-    res  = build_method(method: method, uri: uri, parameters: parameters)
-
-    res.content_type = format  unless format.empty?
-    res.body         = content unless content.empty?
-    answer = http.request(res)
-
-    result = []
-    result << {key: :code, value: answer.code}
-    result << {key: :content_type, value: answer['content-type']}    if answer.key? 'content-type'
-    result << {key: :content_length, value:answer['content-length']} if answer.key? 'content-length'
-    result << {key: :body, value: answer.body}
-    result << {key: :uri, value: answer.uri}
-    result << {key: :conten_range, value: answer.content_range}      if answer.content_range
-    result << {key: :range, value: answer.range}                     if answer.range
-    result << {key: :range_length, value: answer.range_length}       if answer.range_length
-    result << {key: :chunked?, value: answer.chunked?}
-    result << {key: :length, value: answer.length}
-    result << {key: :sub_type, value: answer.sub_type}
-    result << {key: :msg, value: answer.msg}
-    answer.to_hash.each_pair do |k,v|
-      result << {key: k, value: v}
-    end
-    result.to_json
-  #rescue => e
-  #  {error: "Exception was raised: #{e.message}"}.to_json
   end
 
   post '/request' do
